cfe13f9f0a896aba5f1e31d24eec66678e31c44d
muzixing <350959853@qq.com>
2014-12-14 12:24:05 +0800

    change the

--- ovsinstallhelper.sh
+++ ovsinstallhelper.sh
@@ -41,14 +41,22 @@ make modules_install
 mkdir -p /usr/local/etc/openvswitch
 ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema  2>/dev/null
 
-ovsdb-server -v --remote=punix:/usr/local/var/run/openvswitch/db.sock \
-             --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
-             --private-key=db:Open_vSwitch,SSL,private_key \
-             --certificate=db:Open_vSwitch,SSL,certificate \
-             --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
-             --pidfile --detach
+# start ovs server
 
-ovs-vsctl --no-wait init
-ovs-vswitchd --pidfile --detach
+#sh /usr/local/share/openvswitch/scripts/ovs-ctl stop
+sh /usr/local/share/openvswitch/scripts/ovs-ctl restart
+
+# you can use the below commands instead.
+
+#ovsdb-server -v --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+#             --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+#             --private-key=db:Open_vSwitch,SSL,private_key \
+#             --certificate=db:Open_vSwitch,SSL,certificate \
+#             --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
+#             --pidfile --detach
+
+#ovs-vsctl --no-wait init
+#ovs-vswitchd --pidfile --detach
+
 ovs-vsctl show
 depmod -A openvswitch
