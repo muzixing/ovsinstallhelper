e3e4a792a83d459ed5000a88ec928c68e905dae8
None <None>
2015-09-13 16:27:22 +0800

    parsing arg to install any version ovs

--- ovsinstallhelper.sh
+++ ovsinstallhelper.sh
@@ -6,29 +6,35 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+version = "2.3.1"
+if ["$1" != "" ]; then
+   version = $1
+fi
+
+
 #install
 #apt-get update
 #apt-get install -y build-essential
 
 
-echo "====================INSTALL OpenvSwitch-2.3.0===================="
+echo "====================INSTALL OpenvSwitch-$1===================="
 #apt-get install -y   uml-utilities libtool python-qt4 python-twisted-conch debhelper python-all
 
-if [  -f openvswitch-2.3.0.tar.gz ]
+if [  -f openvswitch-$1.tar.gz ]
 then 
-    echo "openvswitch-2.3.0.tar.gz has exist"
+	echo "openvswitch-$1.tar.gz has exist"
 else
-    wget http://openvswitch.org/releases/openvswitch-2.3.0.tar.gz
+	wget http://openvswitch.org/releases/openvswitch-$1.tar.gz
 fi
 
-if [  -d openvswitch-2.3.0 ]
+if [  -d openvswitch-$1 ]
 then
-   rm -r openvswitch-2.3.0
+   rm -r openvswitch-$1
 fi
-tar -xzf openvswitch-2.3.0.tar.gz
+tar -xzf openvswitch-$1.tar.gz
 
 # Install openvswitch
-cd openvswitch-2.3.0
+cd openvswitch-$1
 make clean
 ./configure --with-linux=/lib/modules/`uname -r`/build 2>/dev/null
 make && make install
@@ -41,22 +47,18 @@ make modules_install
 mkdir -p /usr/local/etc/openvswitch
 ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema  2>/dev/null
 
-# start ovs server
+ovsdb-server -v --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+                 --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+                 --private-key=db:Open_vSwitch,SSL,private_key \
+                 --certificate=db:Open_vSwitch,SSL,certificate \
+                 --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
+                 --pidfile --detach
 
-#sh /usr/local/share/openvswitch/scripts/ovs-ctl stop
-sh /usr/local/share/openvswitch/scripts/ovs-ctl restart
+#ovsdb-server -v --remote=punix:/usr/local/var/run/openvswitch/db.sock --pidfile --detach --log-file
 
-# you can use the below commands instead.
-
-#ovsdb-server -v --remote=punix:/usr/local/var/run/openvswitch/db.sock \
-#             --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
-#             --private-key=db:Open_vSwitch,SSL,private_key \
-#             --certificate=db:Open_vSwitch,SSL,certificate \
-#             --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
-#             --pidfile --detach
-
-#ovs-vsctl --no-wait init
-#ovs-vswitchd --pidfile --detach
+ovs-vsctl --no-wait init
+ovs-vswitchd --pidfile --detach
 
 ovs-vsctl show
+
 depmod -A openvswitch
